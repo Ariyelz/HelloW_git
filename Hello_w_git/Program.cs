44d2815f4318a685ae5abf685bc585b216f74f28
Ariyelz <line_bratli@hotmail.com>
2024-01-18 14:33:16 +0100

    added method

--- Hello_w_git/Program.cs
+++ Hello_w_git/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("I hope this works?");
+    }
+    static void CountTo(int num)
+    {
+        Console.WriteLine("I don't know what unexpected token means");
+        Console.WriteLine("so i looked it up, and then i fixed it");
     }
 }
+
